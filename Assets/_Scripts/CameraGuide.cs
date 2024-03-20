ddd19b7fb2a75f1a8ef6901ef68ce383db220ba2
Brycen Addison <b@brycenaddison.com>
2024-03-20 15:35:51 -0400

    feat: Add drag

--- Assets/_Scripts/CameraGuide.cs
+++ Assets/_Scripts/CameraGuide.cs
@@ -1,26 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.SceneManagement;
 using UnityEngine;
-using static UnityEditor.PlayerSettings;
 
 public class CameraGuide : MonoBehaviour
 {
+    public enum MouseControlType
+    {
+        Drag,
+        Off
+    }
+
     [Header("Inscribed")]
-    public bool enableMouseControl = true;
+    public MouseControlType mouseControl = MouseControlType.Drag;
     public float speed = 5.0f;
     public float xMin = -8.5f;
     public float xMax = 8.5f;
     public float zMin = -4.5f;
     public float zMax = 4.5f;
-    Plane plane = new Plane(Vector3.down, Vector3.zero);
+    public Plane plane = new Plane(Vector3.down, Vector3.zero);
 
     [Header("Dynamic")]
-    private Vector2 dragOrigin;
+    private Vector3 dragOrigin;
     private bool cameraDragging = false;
-    private float cameraDistance;
 
-    private Vector3 boundPosition(Vector3 pos)
+    private Vector3 BoundPosition(Vector3 pos)
     {
         pos.x = Mathf.Clamp(pos.x, xMin, xMax);
         pos.y = 0;
@@ -28,22 +29,33 @@ public class CameraGuide : MonoBehaviour
         return pos;
     }
 
-    private void Start()
+    private Vector3 GetPointAtScreenPosition()
     {
-        cameraDistance = Camera.main.WorldToScreenPoint(transform.position).z;
+        return GetPointAtScreenPosition(Input.mousePosition);
     }
-    // Update is called once per frame
+
+    private Vector3 GetPointAtScreenPosition(Vector3 screenPosition)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(screenPosition);
+
+        if (plane.Raycast(ray, out float distance))
+        {
+            return ray.GetPoint(distance);
+        }
+
+        return new Vector3();
+    }
+
+
     void Update()
     {
-        if (enableMouseControl)
+        if (mouseControl == MouseControlType.Drag)
         {
 
             if (Input.GetMouseButtonDown(0))
             {
-                dragOrigin = Input.mousePosition;
+                dragOrigin = GetPointAtScreenPosition();
                 cameraDragging = true;
-
-                Debug.Log("DragOrigin: " + dragOrigin);
 
                 return;
             }
@@ -55,21 +67,9 @@ public class CameraGuide : MonoBehaviour
 
             if (cameraDragging)
             {
-                Vector2 diff = new Vector2(Input.mousePosition.x, Input.mousePosition.y) - dragOrigin;
+                Vector3 diff = GetPointAtScreenPosition() - dragOrigin;
 
-                Ray ray = Camera.main.ScreenPointToRay(diff);
-
-                if (plane.Raycast(ray, out float distance))
-                {
-                    Vector3 move = ray.GetPoint(distance);
-                    Debug.Log("hitPoint: " + move);
-                }
-
-// Vector3 move = Camera.main.ScreenToWorldPoint(new Vector3(diff.x, diff.y, cameraDistance));
-
-// Debug.Log("move: " + move);
-
-              // transform.position = boundPosition(transform.position - move);
+                transform.position = BoundPosition(transform.position - diff);
 
                 return;
             }
@@ -84,6 +84,6 @@ public class CameraGuide : MonoBehaviour
         pos.x += hAxis * speed * Time.deltaTime;
         pos.z += vAxis * speed * Time.deltaTime;
 
-        transform.position = boundPosition(pos);
+        transform.position = BoundPosition(pos);
     }
 }
--- Assets/_Scripts/GuideFollower.cs
+++ Assets/_Scripts/GuideFollower.cs
@@ -6,11 +6,14 @@ public class GuideFollower : MonoBehaviour
     public GameObject target;
     public float distance = 10.0f;
     public float angle = 45.0f;
+    // public float minDistance = 5.0f;
 
-    public float smoothTime = 0.05f;
 
-    [Header("Dynamic")]
-    public Vector3 currentVelocity;
+    // public float smoothTime = 0.05f;
+
+    // [Header("Dynamic")]
+    // public Vector3 currentVelocity;
+
     void LateUpdate()
     {
         if (!target)
@@ -21,11 +24,15 @@ public class GuideFollower : MonoBehaviour
         Vector3 offset = Quaternion.Euler(angle, 0, 0) * Vector3.back * distance;
         Vector3 newPosition = new Vector3(target.transform.position.x, 0, target.transform.position.z) + offset;
 
-        transform.position = Vector3.SmoothDamp(
-            transform.position,
-            newPosition,
-            ref currentVelocity,
-            smoothTime
-        );
+        // TODO: revisit interpolation
+        //
+        // transform.position = Vector3.SmoothDamp(
+        //     transform.position,
+        //     newPosition,
+        //     ref currentVelocity,
+        //     smoothTime
+        // );
+
+        transform.position = newPosition;
     }
 }
