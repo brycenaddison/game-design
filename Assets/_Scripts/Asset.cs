4b30b65c41761b5266e754d4659f061ebbc50322
Brycen Addison <b@brycenaddison.com>
2024-03-27 16:27:05 -0400

    feat: Add asset owner, power assets and customer assets

---
+++ Assets/_Scripts/Asset.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asset : MonoBehaviour
+{
+    public AssetOwner Owner { get; set; }
+}
---
+++ Assets/_Scripts/CustomerAsset.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CustomerAsset : Asset
+{
+    public float Draw;
+    public float Payment;
+
+}
