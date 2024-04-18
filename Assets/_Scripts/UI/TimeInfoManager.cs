34885ecc041d26f0a74989c382d08d3ff2c25a7c
Brycen Addison <b@brycenaddison.com>
2024-04-18 00:36:23 -0400

    feat: Change to month timescale

--- Assets/_Scripts/UI/TimeInfoManager.cs
+++ Assets/_Scripts/UI/TimeInfoManager.cs
@@ -5,22 +5,16 @@ using UnityEngine.UI;
 
 public class TimeInfoManager : MonoBehaviour
 {
-    public GameObject date;
-    public GameObject time;
-
-    private Text dateText;
-    private Text timeText;
     private GameTime gameTime;
+    private Text dateText;
 
     void Start()
     {
-        dateText = date.GetComponent<Text>();
-        timeText = time.GetComponent<Text>();
+        dateText = gameObject.GetComponent<Text>();
         gameTime = Camera.main.GetComponent<GameTime>();
     }
     void Update()
     {
         dateText.text = gameTime.GetDateString();
-        timeText.text = $"{gameTime.GetHour()}:{gameTime.GetMinute().ToString().PadLeft(2, '0')} {gameTime.GetAmPm()}";
     }
 }
