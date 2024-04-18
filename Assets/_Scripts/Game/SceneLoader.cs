7a8ae9a3807900c94f5163226d1a4992a380c74f
Brycen Addison <b@brycenaddison.com>
2024-04-18 13:13:12 -0400

    fix: Start button not working

--- Assets/_Scripts/Game/SceneLoader.cs
+++ Assets/_Scripts/Game/SceneLoader.cs
@@ -8,16 +8,22 @@ public class SceneLoader : MonoBehaviour
 {
     public static void LoadGame()
     {
+        Debug.Log("Loading game");
         SceneManager.LoadScene("PowerStruggle");
+        Debug.Log("Loaded game");
     }
 
     public static void LoadTitle()
     {
+        Debug.Log("Loading title");
         SceneManager.LoadScene("Title Screen");
+        Debug.Log("Loaded title");
     }
 
     public static void LoadStory()
     {
+        Debug.Log("Loading story");
         SceneManager.LoadScene("Story");
+        Debug.Log("Loaded story");
     }
 }
