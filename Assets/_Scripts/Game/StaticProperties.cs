3ceaf67804aff51ffc4c44f3743523413fe4e779
CosmosSolitarus <156846497+CosmosSolitarus@users.noreply.github.com>
2024-04-22 18:24:49 -0400

    Brought over useful changes from JMT2

--- Assets/_Scripts/Game/StaticProperties.cs
+++ Assets/_Scripts/Game/StaticProperties.cs
@@ -1,10 +1,42 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
 public class StaticProperties
 {
     private static string _name = "Unnamed Player";
+    private static Color _color = new Color(0, 0, 0);
+    private static int _mapSize = 12;
+    private static int _numAIs = 3;
+    private static float _strengthOfAI = 0.8f;
 
     public static string Name
     {
         get => _name;
         set => _name = value;
+    }
+
+    public static Color Color
+    {
+        get => _color;
+        set => _color = value;
+    }
+
+    public static int MapSize
+    {
+        get => _mapSize;
+        set => _mapSize = value;
+    }
+
+    public static int NumAIs
+    {
+        get => _numAIs;
+        set => _numAIs = value;
+    }
+
+    public static float StrengthOfAI
+    {
+        get => _strengthOfAI;
+        set => _strengthOfAI = value;
     }
 }
